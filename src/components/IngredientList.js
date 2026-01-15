d29ca44875fbe77823be1c93b593607605b8835b
IsshinTheSwordSaint <ut_4003@zsp3zamosc.pl>
2026-01-15 08:25:43 +0100

    sprawdzian

--- src/components/IngredientList.js
+++ src/components/IngredientList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import 'c:/Users/4Mgr1/sprawdzian/src/IngredientList.css';
+import '../IngredientList.css';
 
 const IngredientList = ({ingredients}) => {
     const defaultMessage = "Brak składników do wyświetlenia";
--- src/components/StepsList.js
+++ src/components/StepsList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import 'c:/Users/4Mgr1/sprawdzian/src/IngredientList.css';
+import '../IngredientList.css';
 
 const StepsList = ({steps}) => {
     const defaultMessage = "Brak kroków do wyświetlenia";
